dff1a5a06fddf7de07327312d4d79425247476ee
Tauri Ramst <gittauri19@users.noreply.github.com>
2020-09-16 12:38:20 +0300

    muudetud directoryd

--- main/testleht.php
+++ main/testleht.php
@@ -23,6 +23,7 @@ if($hournow >= 18 and $hournow <= 20){
 if($hournow >= 20 and $hournow <= 21){
 	$partofday = "aeg õhtutoidu tegemiseks";
 }
+
 // see märk näitab kommentaari "//"
 //vaatame semsetri kulgemist
 $semesterstart = new DateTime ("2020-8-31");
@@ -36,8 +37,36 @@ $today = new DateTime ("now");
 //tänase ja semestri erinevus jne jne 
 //kui aga päevade arv on miinuses siis näiteks: if($fromsemesterstartdays < 0){semester pole alanud}
 //veelgi if ($semesteridurationdays > $semesterduration){"semester on läbi, naudi vaba aega."}
+   
+ //alates sellest tekstist kuni php lõpuni on kopeeritud õpetaja lehelt. 
+ //vajab ise harjutamist.
  
-
+   $today = new DateTime("now");
+  //if($fromsemesterstartdays < 0){semester pole peale hakanud}
+  $fromsemesterstart = $semesterstart->diff($today);
+  //saime aja erinevuse objektina, seda niisama näidata ei saa
+  $fromsemesterstartdays = $fromsemesterstart->format("%r%a");
+  $semesterpercentage = 0;
+  
+  $semesterpercentage = 0;
+    
+  $semesterinfo = "Semester kulgeb vastavalt akadeemilisele kalendrile.";
+if($semesterstart > $today){
+	  $semesterinfo = "Semester pole veel peale hakanud!";
+  }
+  if($fromsemesterstartdays == 0){
+	  $semesterinfo = "Semester algab täna!";
+  }
+  if($fromsemesterstartdays > 0 and $fromsemesterstartdays < $semesterdurationdays){
+	  $semesterpercentage = ($fromsemesterstartdays / $semesterdurationdays) * 100;
+	  $semesterinfo = "Semester on täies hoos, kestab juba " .$fromsemesterstartdays ." päeva, läbitud on " .round($semesterpercentage,3)  ."%.";
+  }
+  if($fromsemesterstartdays == $semesterdurationdays){
+	  $semesterinfo = "Semester lõppeb täna!";
+  }
+  if($fromsemesterstartdays > $semesterdurationdays){
+	  $semesterinfo = "Semester on läbi saanud!";
+  }
 ?>
 
 <!DOCTYPE html>
@@ -49,20 +78,17 @@ $today = new DateTime ("now");
 </head>
 <body>
   <h1>testi leht</h1>
-  <p>See veebileht on loodud õppetöö kaigus <?php echo $username1; ?> poolt ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p>See veebileht on loodud õppetöö käigus <?php echo $username1; ?> poolt ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
   <p>uuesti</p>
   
   <p> see veebi leht on <a href="https://www.tlu.ee"> tähtis </a>  </p>
-  
-  <p> see j2rgnev tekst p2rineb yhikast kodut88ga seoses ning pean t6dema, et siiani j6udmine, et seda teksti kirjutada v6ttis ~2H.... </p>
-  
-  <p> Oleme ausad. see on SEE ON 2H LIIGA PALJU! </p>
   
   <p> Palun vaadake juhendid yle. juhendeid on 3-4tk ja r22givad yksteisele vastu </p>
   
   <p> Lehe avamise hetkel oli kuupäev ja kellaaeg: <?php echo $fulltimenow; ?> </p>
   
   <p><?php echo " parajasti on " . $partofday ."."; ?> </p>
+  <p><?php echo $semesterinfo; ?></p>
 </body>
 </html>
 
